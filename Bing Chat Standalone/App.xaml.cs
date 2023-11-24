6013396251be5d08cac1372dbc7be559c0e5b766
Drew Chase <dcmanproductions@gmail.com>
2023-11-24 17:15:06 -0500

    Add project files.

---
+++ Bing Chat Standalone/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Bing_Chat_Standalone
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Bing Chat Standalone/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿// LFInteractive LLC. 2021-2024
+﻿using System.Text;
+using System.Windows;
+
+namespace Bing_Chat_Standalone
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            browser.Source = new System.Uri("https://www.bing.com/chat");
+        }
+    }
+}
